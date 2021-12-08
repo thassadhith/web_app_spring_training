4a87df2f5fb22f788f41563d32da520c6ac45424
thassadhith <sissilarbi19@gmail.com>
2021-12-08 14:23:22 +0100

    partie6

--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -1,4 +1,25 @@
+package fr.lernejo.todo;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+@RestController
+public class TodoListController {
+    private final ArrayList<Todo> listtodo = new ArrayList<Todo>();
 
 
+    @PostMapping("api/todo")
+    public void add(Todo mynewtodo) {
+
+    }
+
+    @GetMapping("api/todo")
+    public ArrayList<Todo> getlist(){
+        listtodo.add(new Todo("salut tout le monde","ss"));
+        return listtodo;
+    }
+}
